5055331a59a1e3da8d4891af4815253311a36991
Thibaut Dubernet <thibaut@dubernet.ch>
2018-08-20 22:47:42 +0200

    do post request (untested)

---
+++ src/main/kotlin/org/matsim/usagestats/UsageStatsConfigGroup.kt
@@ -0,0 +1,13 @@
+package org.matsim.usagestats
+
+import org.matsim.core.config.ReflectiveConfigGroup
+
+class UsageStatsConfigGroup : ReflectiveConfigGroup(GROUP_NAME) {
+    @get:StringGetter("endpoints")
+    @set:StringSetter("endpoints")
+    var endpoints: List<String> = emptyList()
+
+    companion object {
+        val GROUP_NAME = "usageStats"
+    }
+}
--- src/main/kotlin/org/matsim/usagestats/listeners/StatsListener.kt
+++ src/main/kotlin/org/matsim/usagestats/listeners/StatsListener.kt
@@ -2,10 +2,14 @@ package org.matsim.usagestats.listeners
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.log4j.Logger
 import org.matsim.core.controler.events.ShutdownEvent
 import org.matsim.core.controler.listener.ShutdownListener
 import org.matsim.usagestats.UsageStats
+import org.matsim.usagestats.UsageStatsConfigGroup
 
 /**
  * @author thibautd
@@ -20,7 +24,21 @@ class StatsListener: ShutdownListener {
                     .setDefaultPropertyInclusion(JsonInclude.Include.NON_EMPTY)
                     .writerWithDefaultPrettyPrinter()
 
-            println(jsonWriter.writeValueAsString(stats))
+            val json = jsonWriter.writeValueAsString(stats)
+
+            val configGroup = event.services.config.getModule(UsageStatsConfigGroup.GROUP_NAME) as UsageStatsConfigGroup
+
+            HttpClientBuilder.create().build().use {
+                for (endpoint in configGroup.endpoints) {
+                    val request = HttpPost(endpoint)
+                    request.entity = StringEntity(json)
+                    request.addHeader("content-type", "application/json")
+
+                    val response = it.execute(request)
+                    // be smarter with response?
+                    log.debug("Transmitting usage data to $endpoint resulted in status ${response.statusLine?.statusCode}")
+                }
+            }
         }
         catch (t: Throwable) {
             log.debug("problem sending usage statistics.", t)
