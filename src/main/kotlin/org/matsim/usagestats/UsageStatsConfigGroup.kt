fa4c2193a6da7d0a45402265c8e0e2c1ab1906d1
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2018-08-21 10:57:14 +0200

    fix config group: List is not a valid stringable type

--- src/main/kotlin/org/matsim/usagestats/UsageStatsConfigGroup.kt
+++ src/main/kotlin/org/matsim/usagestats/UsageStatsConfigGroup.kt
@@ -1,11 +1,23 @@
 package org.matsim.usagestats
 
 import org.matsim.core.config.ReflectiveConfigGroup
+import org.matsim.core.utils.collections.CollectionUtils
 
 class UsageStatsConfigGroup : ReflectiveConfigGroup(GROUP_NAME) {
-    @get:StringGetter("endpoints")
-    @set:StringSetter("endpoints")
-    var endpoints: List<String> = listOf("http://ivt-mokil.ethz.ch/api/data")
+    val endpoints: MutableSet<String> = mutableSetOf("http://ivt-mokil.ethz.ch/api/data")
+
+    fun addEndpoint(url: String) {
+        endpoints.add(url)
+    }
+
+    @StringGetter("endpoints")
+    private fun getEndpointsString(): String = CollectionUtils.setToString(endpoints)
+
+    @StringSetter("endpoints")
+    private fun setEndpointsString(v: String) {
+        endpoints.clear()
+        endpoints.addAll(CollectionUtils.stringToSet(v))
+    }
 
     companion object {
         val GROUP_NAME = "usageStats"
